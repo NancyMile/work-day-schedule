8dba483dc5d1348ecffa362ac019d7f8e04d7599
nancy <smithnancy@outlook.com>
2022-09-10 08:57:23 +1000

    asign id  fixed  and setting  and getting  localstored  fixed

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -14,7 +14,6 @@ divForm.append(blockForm);
 blockForm.append(divTimeBlock, divDescription, divBtn);
 divDescription.append(inputForm);
 divBtn.append("<button type='submit' class='btn btn-primary'><i class='bi bi-archive'></i>Save</button>");
-
 // function handle date to be displayed on header
 function handleDate(){
     var now = moment().format("dddd MMMM Do YYYY hh:mm:ss a");
@@ -51,12 +50,9 @@ function changebackground(blockTime){
 //fuction save description
 function saveDescription(event){
    event.preventDefault();
-   var a = JSON.stringify(event.target.id);
-   var new_a = a.substring(5);
-   var nueva = '"#'+new_a;
-   console.log("save description function");
-   localStorage.setItem("description", a);
-   localStorage.setItem("segundo", nueva);
+   var id = event.target.id;
+   var textfield = document.getElementById('input_'+id)
+   localStorage.setItem("description_"+id, textfield.value);
 }
 
 //Defining time blocks for business hours.
@@ -64,17 +60,42 @@ function saveDescription(event){
 for(j= 9; j<= 17; j++){
     //text with 12h format for the time colum
     var text = moment(j,"h").format("h A");
+    var id = moment(j,"h").format("ha");
     //hour in 24 format to be compared
     var hour =parseInt(moment(j,"H").format("H"));
     divTimeBlock.text(text);
     //add attribute id  to the form
-    blockForm.attr("id","des_"+text);
+    blockForm.attr("id", id);
     //set some attributes id and onclick event
-    inputForm.attr("id",text);
+    inputForm.attr("id", "input_"+id);
+    inputForm.attr("value", window.localStorage.getItem("description_"+id));
     //blockForm.attr("onSubmit",saveDescription(this));
     //append the divBlock
     addDiv(hour);
 }
 
+// This function is being called below and will run when the page loads.
+function init() {
+   // Get stored description from localStorage
+    var description = JSON.parse(localStorage.getItem("description"));
+    // If description were retrieved from localStorage, update the weather array to it
+    if (storedDescription !== null) {
+      description = storedDescription;
+    }
+    // This is a helper function that will render description to the DOM
+ // renderDescription();
+}
+
+function storedDescription (){
+    // Stringify and set key in localStorage to weather array
+    localStorage.setItem("weather", JSON.stringify(weather));
+}
+
 //calling handledate
 setInterval(handleDate,1000);
+
+// Calls init to retrieve data and render it to the page on load
+window.onload = function() {
+    init()
+    //console.log('loading the things');
+}
